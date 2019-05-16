4a055a2fb55a6f7e309bed78a16133969ab9eafd
KFad <kfadigan@yahoo.com>
2019-05-16 09:50:36 -0400

    learn about generators

---
+++ db/migrate/20190516134709_create_accounts.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateAccounts < ActiveRecord::Migration
+  def change
+    create_table :accounts do |t|
+      t.string :name
+      t.string :payment_status
+
+      t.timestamps null: false
+    end
+  end
+end
